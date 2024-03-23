b75e47b0233f907b81324216748454181c0c6a4b
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-03-23 14:09:28 -0600

    Add notifications to the create ad form.

--- ad-creation.js
+++ ad-creation.js
@@ -1,6 +1,7 @@
 import { session } from "./utils/session.js";
 import { navbarController } from "./navbar/navbar-controller.js";
 import { adCreationController } from "./ad-creation/ad-creation-controller.js";
+import { notificationController } from "./notification/notification-controller.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   const { validateAuthentication } = session();
@@ -9,6 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.querySelector('#navbar');
   navbarController(navbar);
 
-  const adCreation = document.querySelector('#ad-creation-form');
-  adCreationController(adCreation);
+  const adCreationForm = document.querySelector('#ad-creation-form');
+
+  const notifications = document.querySelector('#notifications');
+	const { showNotification } = notificationController(notifications);
+	adCreationForm.addEventListener('ad-creation-notification', (event) => {
+		event.stopPropagation();
+		showNotification(event.detail.message, event.detail.type, event.detail.autoRemove);
+	});
+
+  adCreationController(adCreationForm);
 });
--- ad-creation/ad-creation-controller.js
+++ ad-creation/ad-creation-controller.js
@@ -1,11 +1,12 @@
 import { createAd } from "./ad-creation-model.js";
+import { dispatchEvent } from "../utils/dispatchEvent.js";
 
-export function adCreationController(adCreation) {
+export function adCreationController(adCreationForm) {
 	
-	adCreation.addEventListener('submit', async (event) => {
+	adCreationForm.addEventListener('submit', async (event) => {
 		event.preventDefault();
 	
-		const formData = new FormData(adCreation);
+		const formData = new FormData(adCreationForm);
 		const name = formData.get("name");
 		const description = formData.get("description");
 		const image = formData.get("image");
@@ -13,12 +14,23 @@ export function adCreationController(adCreation) {
 		const sale = formData.get("sale");
 		
 		try {
-		  await createAd(name, description, image, price, sale === '1');
-		  setTimeout(() => {
-			window.location = "index.html";
-		  }, 2000);
+			await createAd(name, description, image, price, sale === '1');
+			
+			dispatchEvent('ad-creation-notification', {
+				message: 'El anuncio se creó correctamente.',
+				type: 'success',
+				autoRemove: true
+			}, adCreationForm);
+
+			setTimeout(() => {
+				window.location = "index.html";
+			}, 2000);
 		} catch (error) {
-		  alert(error);
+			dispatchEvent('ad-creation-notification', {
+				message: error,
+				type: 'error',
+				autoRemove: true
+			}, adCreationForm);
 		}
 	});
 
