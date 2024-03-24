f80766f18de194e08d68f0a4871fb533daa39e2a
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-03-24 00:45:54 -0600

    Create the loader controller and view.

---
+++ loader/loader-controller.js
@@ -0,0 +1,19 @@
+import { buildLoader } from './loader-view.js'
+
+export const loaderController = (loader) => {
+	
+	const showLoader = () => {
+		loader.classList.add('active');
+		loader.innerHTML = buildLoader();
+	}
+	
+	const hideLoader = () => {
+		loader.classList.remove('active');
+		loader.innerHTML = '';
+	}
+
+	return {
+		showLoader,
+		hideLoader,
+	};
+}
---
+++ loader/loader-view.js
@@ -0,0 +1,14 @@
+export const buildLoader = () => {
+	return `
+	<div class="spinner-grow spinner-grow-sm" role="status">
+		<span class="visually-hidden">Loading...</span>
+	</div>
+	<div class="spinner-grow spinner-grow-sm" role="status">
+		<span class="visually-hidden">Loading...</span>
+	</div>
+	<div class="spinner-grow spinner-grow-sm" role="status">
+		<span class="visually-hidden">Loading...</span>
+	</div>
+  	`;
+}
+  
